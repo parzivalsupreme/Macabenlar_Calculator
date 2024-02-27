8ecfcae8cc95d8d0adbf3ac5a18a25c98e873173
Elijah Macabenalar <ajah35961@gmail.com>
2024-02-27 20:51:38 +0800

    .

--- app/src/main/java/com/example/macabenlar_calculator/MainActivity.java
+++ app/src/main/java/com/example/macabenlar_calculator/MainActivity.java
@@ -1,13 +1,60 @@
 package com.example.macabenlar_calculator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+  private TextView textView;
+  private String currentNumber = "";
+  private String operation = "";
+  private double result = 0;
+  private boolean isNewOperation = true;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    textView = findViewById(R.id.textView);
+  }
+
+  public void onNumberClick(View view) {
+    if (isNewOperation) {
+      currentNumber = "";
+      isNewOperation = false;
+    }
+
+    String number = ((TextView) view).getText().toString();
+    currentNumber += number;
+    textView.setText(currentNumber);
+  }
+
+  public void onOperationClick(View view) {
+    if (!isNewOperation) {
+      if (!currentNumber.isEmpty()) {
+        double number = Double.parseDouble(currentNumber);
+        if (operation.equals("+")) {
+          result += number;
+        } else if (operation.equals("-")) {
+          result -= number;
+        } else if (operation.equals("*")) {
+          result *= number;
+        } else if (operation.equals("/")) {
+          if (number != 0) {
+            result /= number;
+          } else {
+            textView.setText("Error");
+            return;
+          }
+        }
+        textView.setText(String.valueOf(result));
+      }
+    }
+
+    operation = ((TextView) view).getText().toString();
+    isNewOperation = true;
   }
 }
